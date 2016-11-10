5c8677c8532fe9ebe9bb333ed519c7b0fef1b61a
nguyendinhhuynhbk <nguyendinhhuynhbk@gmail.com>
2016-11-10 18:41:31 +0700

    edit phone v2

--- app/src/main/java/a8/group/ttnm/x/view/PhoneCalling.java
+++ app/src/main/java/a8/group/ttnm/x/view/PhoneCalling.java
@@ -3,6 +3,8 @@ package a8.group.ttnm.x.view;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -113,5 +115,21 @@ public class PhoneCalling extends AppCompatActivity implements View.OnClickListe
         editText.setFocusable(true);
         editText.setKeyListener(null);
         editText.setBackgroundColor(Color.TRANSPARENT);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
